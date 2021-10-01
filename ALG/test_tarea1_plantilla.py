4c5a793db1a26f2849135c71414ff7b236fe368c
Fabián Scherle <fscherle98@gmail.com>
2021-10-02 01:00:15 +0200

    General update, queda intermediate

--- ALG/test_tarea1_plantilla.py
+++ ALG/test_tarea1_plantilla.py
@@ -1,6 +1,4 @@
-#import numpy as np
-
-##INCOMPLETO#######################################################
+#########################################################
 def dp_levenshtein_backwards(x, y) :
     lengthX = len(x)
     lengthY = len(y)  # m
@@ -43,31 +41,52 @@ def dp_restricted_damerau_backwards(x, y) :
                 D.append(prev2[j-2] + 1)
             # Se añade el mínimo de las distancias a current
             current.append(min(D))
-        #print(current)
-    return current[leny]
+    return current[-1]
 
 
 ##INCOMPLETO#######################################################
 def dp_intermediate_damerau_backwards(x, y) :
-    return 0
-    lenx = len(x); leny = len(y)
+    lenx = len(x) # longitud de la cadena x
+    leny = len(y) # longitud de la cadena y
     cte = 1
-    # matriz tendrá tantas filas como caracteres en x
-    # y tantas columnas como caracteres en y 
-    matriz = np.zeros( (lenx, leny) , dtype=int)
-    for i in range(0, lenx) : 
-        for j in range(0, leny) :
-            D = []
-            if i==0 and j==0 : D.append(0)
-            if i>0 : D.append(matriz[i-1][j] + 1)
-            if j>0 : D.append(matriz[i][j-1] + 1)
-            if i>0 and j>0 : 
-                D.append(matriz[i-1][j-1] + (x[i]!=y[j]))
-            # Condicion adicional
-            # if i+j+1 < np.abs(i-j) : matriz[i-1][j-1]
-            matriz[i][j] = min(D)
-    return matriz[lenx-1][leny-1]
 
+    return 0
+    # current, prev1, prev2 y prev3 almacenaran las distancias de cada caracter
+    # de forma eficiente
+    prev1 = []; prev1 = []; current = []
+    for i in range(0, lenx+1) : 
+        prev3 = prev2; prev2 = prev1; prev1 = current; current = []
+        for j in range(0, leny+1) :
+            D=[]
+            if i==0 and j == 0 : D.append(0)
+            if i > 0 : D.append(prev1[j] + 1)
+            if j > 0 : D.append(current[j-1] + 1)
+            if i > 0 and j > 0 :
+                D.append(prev1[j-1] + (x[i-1] != y[j-1]))
+            if i > 1 and j > 1 and x[i-2] == y[j-1] and x[i-1] == y[j-2] :
+                D.append(prev2[j-2] + 1)
+
+            # Condiciones adicionales:
+            # Teniendo el ejemplo de ab y bca :
+            elif i > 1 and j > 1+cte and x[i-2] == y[j-1] and x[i-1] == y[j-2-cte] : 
+                #print("ab-bca", i, j) 
+                D.append = 1 + min(dl2[j-1],   # Insertar 
+                                dl1[j],      # Eliminar 
+                                dl1[j-1],    # Reemplazar 
+                                dl0[j-3]+1)  # Intercambiar  
+            # Y lo contrario, bca y ab :
+            elif i > 1+cte and j > 1 and x[i-2-cte] == y[j-1] and x[i-1] == y[j-2] : 
+                #print("bca-ab", i, j) 
+                dl2[j] = 1 + min(dl2[j-1],   # Insertar 
+                                dl1[j],      # Eliminar 
+                                dl1[j-1],    # Reemplazar 
+                                dl4[j-2]+1)  # Intercambiar
+
+            # Se añade el mínimo de las distancias a current
+            current.append(min(D))
+    return current[-1]
+
+#########################################################
 test = [("algoritmo","algortimo"),
         ("algoritmo","algortximo"),
         ("algoritmo","lagortimo"),
