e860ca5db4112f6f19d6562bf6f71bb3c784c2c3
Fabián Scherle <fscherle98@gmail.com>
2021-10-02 10:31:25 +0200

    Update test_tarea1

--- ALG/test_tarea1_plantilla.py
+++ ALG/test_tarea1_plantilla.py
@@ -1,4 +1,6 @@
-#########################################################
+#############################################################################################
+#    Distancia de Levenstein
+#############################################################################################
 def dp_levenshtein_backwards(x, y) :
     lengthX = len(x)
     lengthY = len(y)  # m
@@ -20,7 +22,9 @@ def dp_levenshtein_backwards(x, y) :
 
     return current[-1]
 
-#########################################################
+#############################################################################################
+#    Distancia de Damerau-Levenstein restringida
+#############################################################################################
 def dp_restricted_damerau_backwards(x, y) :
     lenx = len(x) # longitud de la cadena x
     leny = len(y) # longitud de la cadena y
@@ -43,17 +47,17 @@ def dp_restricted_damerau_backwards(x, y) :
             current.append(min(D))
     return current[-1]
 
-
-##INCOMPLETO#######################################################
+#############################################################################################
+#    Distancia de Damerau-Levenstein intermedia
+#############################################################################################
 def dp_intermediate_damerau_backwards(x, y) :
     lenx = len(x) # longitud de la cadena x
     leny = len(y) # longitud de la cadena y
-    cte = 1
+    cte = 1 # constante prefijada a 1 considerando coste(acb, ba)=2 y coste(ab, bca)=2
 
-    return 0
     # current, prev1, prev2 y prev3 almacenaran las distancias de cada caracter
     # de forma eficiente
-    prev1 = []; prev1 = []; current = []
+    prev2 = []; prev1 = []; current = []
     for i in range(0, lenx+1) : 
         prev3 = prev2; prev2 = prev1; prev1 = current; current = []
         for j in range(0, leny+1) :
@@ -67,26 +71,21 @@ def dp_intermediate_damerau_backwards(x, y) :
                 D.append(prev2[j-2] + 1)
 
             # Condiciones adicionales:
-            # Teniendo el ejemplo de ab y bca :
+            # Teniendo el ejemplo de x=ab e y=bca :
             elif i > 1 and j > 1+cte and x[i-2] == y[j-1] and x[i-1] == y[j-2-cte] : 
-                #print("ab-bca", i, j) 
-                D.append = 1 + min(dl2[j-1],   # Insertar 
-                                dl1[j],      # Eliminar 
-                                dl1[j-1],    # Reemplazar 
-                                dl0[j-3]+1)  # Intercambiar  
-            # Y lo contrario, bca y ab :
+                D.append(prev2[j-3] + 2)
+            # Y lo contrario, x=bca e y=ab :
             elif i > 1+cte and j > 1 and x[i-2-cte] == y[j-1] and x[i-1] == y[j-2] : 
-                #print("bca-ab", i, j) 
-                dl2[j] = 1 + min(dl2[j-1],   # Insertar 
-                                dl1[j],      # Eliminar 
-                                dl1[j-1],    # Reemplazar 
-                                dl4[j-2]+1)  # Intercambiar
+                D.append(prev3[j-2] + 2)
 
             # Se añade el mínimo de las distancias a current
             current.append(min(D))
     return current[-1]
 
-#########################################################
+
+#############################################################################################
+#    Pruebas:
+#############################################################################################
 test = [("algoritmo","algortimo"),
         ("algoritmo","algortximo"),
         ("algoritmo","lagortimo"),
