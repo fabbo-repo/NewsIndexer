031ac06d431337fb52c4a372d4eb9ea493170ce9
Anthony J. Romero Barbera <anrobarbusiness@outlook.com>
2021-11-16 16:22:20 +0100

    Revisar , not okey

--- distances/trie_distances.py
+++ distances/trie_distances.py
@@ -34,7 +34,32 @@ def dp_levenshtein_trie(x, trie, th) :
 #    Restricted Damerau-Levenstein Trie Distance
 #############################################################################################
 def dp_restricted_damerau_trie(x, trie, th) :
-    return {}
+    len_trie = trie.get_num_states()
+    res = {}
+
+    # current and prev are created as lists with len_trie size
+    current = [None] * len_trie
+    prev = [None] * len_trie 
+    current[0] = 0
+
+    # first column is filled
+    for st in range(1,len_trie):
+        current[st] = current[trie.get_parent(st)] + 1
+    
+    for ch in x :
+        prev2 , prev , current = prev, current,[]
+        current[0] = prev[0]+1
+        for st in range(1,len_trie):
+            father = trie.get_parent(st)
+            current[st] = min(prev2[st-1]+1,prev[st]+1,current[father]+1,prev[father] + (ch != trie.get_label(st)) )
+        if(min(current) > th) : return res
+    
+    for st in range(1,len_trie):
+        if trie.is_final(st) and current[st] <= th:
+            res[trie.get_output(st)] = current[st]
+    
+    return res
+
 
 #############################################################################################
 #    Intermediate Damerau-Levenstein Trie Distance
